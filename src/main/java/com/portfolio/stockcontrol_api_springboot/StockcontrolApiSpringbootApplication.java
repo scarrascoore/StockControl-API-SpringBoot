a371d52fd56f046e6334871b35996648f02febbc
scarrascoore <scarrascoore@gmail.com>
2026-05-26 14:39:31 -0500

    chore: configurar estructura inicial de Spring Boot

---
+++ src/main/java/com/portfolio/stockcontrol_api_springboot/StockcontrolApiSpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.portfolio.stockcontrol_api_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockcontrolApiSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockcontrolApiSpringbootApplication.class, args);
+
+	}
+
+}
